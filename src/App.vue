ad8c8124cfd8bfe6861aaca19a7d8343e98223a7
Axel Ridray <axel.ridray@sfr.fr>
2021-06-30 17:49:57 +0200

    add exo branch

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,12 @@
 <template>
   <div id="app">
-    <nav>
-      <h1>Welcome on my first Vue page</h1>
-      <NavLink url="/" text="Home" />
-      <NavLink url="/about" text="About" />
-      <NavLink url="/contact" text="Contact" />
-    </nav>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import NavLink from'./components/HomeLink.vue'
-
-export default {
-  name: 'App',
-  components: {
-    NavLink
-  }
-}
-</script>
 
 <style lang="scss">
 #app {
@@ -27,6 +15,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
